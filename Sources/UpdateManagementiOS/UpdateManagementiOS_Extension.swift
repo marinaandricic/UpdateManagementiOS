a478b05232a2dc89a18c3c08de07ab3ea4258f74
Marina Andricic <marina.andricic@j2.com>
2023-05-19 14:45:49 +0100

    x

--- Sources/UpdateManagementiOS/UpdateManagementiOS_Extension.swift
+++ Sources/UpdateManagementiOS/UpdateManagementiOS_Extension.swift
@@ -178,7 +178,7 @@ extension UpdateManagementiOS {
         if  currentDate == reminderDate!  {
             return self.sessionManager.UpdateManagerReminderShow
         }
-        else if  currentDate <= reminderDate!  {
+        else if  currentDate < reminderDate!  {
             return false
         }  else {
             self.sessionManager.UpdateManagerReminderDate = currentDate
